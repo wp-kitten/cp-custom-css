603d3a86e8b0982841716f24db1199d08741dbcf
wp-kitten <wp.kytten@gmail.com>
2020-10-14 18:14:11 +0300

    Loads custom styles

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
  * Stores the name of the plugin's directory
  * @var string
  */
+
+use App\Helpers\ScriptsManager;
+
 define( 'CPCS_PLUGIN_DIR_NAME', basename( dirname( __FILE__ ) ) );
 /**
  * Stores the system path to the plugin's directory
  * @var string
  */
 define( 'CPCS_PLUGIN_DIR_PATH', trailingslashit( wp_normalize_path( dirname( __FILE__ ) ) ) );
-
 
 function cpcsGetStylesheetUrl()
 {
@@ -22,3 +24,10 @@ if ( cp_is_admin() ) {
     require_once( CPCS_PLUGIN_DIR_PATH . 'admin/hooks.php' );
     require_once( CPCS_PLUGIN_DIR_PATH . 'admin/routes.php' );
 }
+
+/**
+ * Enqueue custom styles
+ */
+add_action( 'contentpress/site/head', function () {
+    ScriptsManager::enqueueStylesheet( 'contentpress-custom-styles', cpcsGetStylesheetUrl() );
+} );
